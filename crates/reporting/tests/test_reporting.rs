6a280ef97cd9267ddbc14f780ed34e516ece4aee
Ayaz Hafiz <ayaz@rwx.com>
2022-10-12 10:50:51 -0500

    Report errors for duplicate bound abilities

--- crates/reporting/tests/test_reporting.rs
+++ crates/reporting/tests/test_reporting.rs
@@ -11457,4 +11457,26 @@ All branches in an `if` must have the same type!
     Note: `Hash` cannot be generated for functions.
     "###
     );
+
+    test_report!(
+        duplicate_ability_in_has_clause,
+        indoc!(
+            r#"
+            f : a -> {} | a has Hash & Hash
+
+            f
+            "#
+        ),
+    @r###"
+    ── DUPLICATE BOUND ABILITY ─────────────────────────────── /code/proj/Main.roc ─
+
+    I already saw that this type variable is bound to the `Hash` ability
+    once before:
+
+    4│      f : a -> {} | a has Hash & Hash
+                                       ^^^^
+
+    Abilities only need to bound to a type variable once in a `has` clause!
+    "###
+    );
 }
